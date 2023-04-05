2372fa3f2873cdce6b58efaea20e592dc24eccc7
Obaloluwa Tubi <Tubiobaloluwa@gmail.com>
2023-04-05 03:37:08 +0100

    Structure done

--- js/script.js
+++ js/script.js
@@ -109,16 +109,24 @@ const tabs = document.querySelectorAll('.tab_btn'),
 
 
 tabs.forEach((tab, index) => {
-    tab.addEventListener('click', () => {
+    tab.addEventListener('click', (e) => {
         tabs.forEach(tab => {
             tab.classList.remove('active');
         });
         tab.classList.add('active');
+        var line = document.querySelector('.line');
+        line.style.width = e.target.offsetWidth + "px";
+        line.style.left = e.target.offsetLeft + "px";
+
+        all_content.forEach(content => {
+            content.classList.remove('active');
+        })
+        all_content[index].classList.add('active');
     });
 });
 
 
-var swiper1 = new Swiper('.content', {
+var swiper1 = new Swiper('.content_box', {
     slidesPerView: 4,
 
     spaceBetween: 30,
